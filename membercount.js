9d384e90c71171040459e0c9deb8b5e05b113546
Ruben de Bruijne <49074962+AxyxDeveloper@users.noreply.github.com>
2020-02-18 19:10:03 +0100

    Update membercount.js

--- membercount.js
+++ membercount.js
@@ -9,5 +9,13 @@ client.on("guildMemberAdd", async function (user) {
 const sendCh = message.guild.channels.find(ch => ch.id === `${channelID});
 if(!sencCh) return;
 sendCh.setName(`Members: ${user.guild.memberCount}`)
-console.log("There is a new member!
+console.log("There is a new member!")
+if(sendCh.type === "text") {
+sendCh.overwritePermissions(message.guild.id, {
+    VIEW_CHANNEL: true,
+    SEND_MESSAGES: false
+} else if(sencCh.type === "voice") {
+    VIEW_CHANNEL: true,
+    CONNECT: false
+}
 )}
