e2913e0d744d8646f9eb84556f2401f64ae639d0
Ruben de Bruijne <49074962+AxyxDeveloper@users.noreply.github.com>
2020-02-19 14:56:33 +0100

    Create pollcmd.js

---
+++ pollcmd.js
@@ -0,0 +1,41 @@
+//you can copy paste this code. it will work on bots that use client. and not bot., You can always replace.
+
+    if(command === "poll") {
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You're not allowed to use this command!") //remove this line of code to allow everyone to use
+        const pollChannel = message.mentions.channels.first()
+        if(!pollChannel) return message.reply("Specify a Channel to send the poll in.");
+        const pollTitle = args[1]
+        if(!pollTitle) return message.reply("Specify a Title!");
+        pollTitle.replace(" ", " ");
+        const pollEmbed1 = new Discord.RichEmbed()
+        .setTitle("Creating Poll...")
+        .setColor("#ff8c00")
+        const pollEmbed2 = new Discord.RichEmbed()
+        .setTitle("Sending Poll...")
+        .setColor("#ff8c00")
+        const pollEmbed3 = new Discord.RichEmbed()
+        .setTitle("Poll Send...")
+        .setDescription("**Adding Reactions**")
+        .setColor("#ff8c00")
+        const pollEmbed4 = new Discord.RichEmbed()
+        .setTitle("Reactions Added")
+        .setColor("#ff8c00")
+        message.channel.send(pollEmbed1).then(poll => {
+            const pollStatus = poll
+            const pollEmbed = new Discord.RichEmbed()
+            .setTitle("Poll")
+            .setDescription(`${pollTitle}`)
+            .setTimestamp()
+            .setColor("#7cfc00")
+            pollStatus.edit(pollEmbed2)
+            pollChannel.send(pollEmbed).then(embed => {
+                pollStatus.edit(pollEmbed3)
+                embed.react("👍") //you can change these  emojis <unicode character>
+                embed.react("👎")
+                pollStatus.edit(pollEmbed4)
+                setTimeout(() => {
+                    pollStatus.delete()
+                }, 8000) //you can change the time of the status message will be deleted
+            })
+        })
+    }
