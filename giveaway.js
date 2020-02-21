f00573e63def782c60dc6d348c8914b27a5b3d84
Ruben de Bruijne <49074962+AxyxDeveloper@users.noreply.github.com>
2020-02-21 16:55:07 +0100

    Some Updates

--- giveaway.js
+++ giveaway.js
@@ -1,54 +1,28 @@
-if (command === "giveaway") {
-        try {
+  if (command === "giveaway") {
         if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have permission!")
-        try {
-        const _channel = message.mentions.channels.first()
-        } catch (error) {
-            const Error = new Discord.RichEmbed()
-            .setTitle("Error")
-            .setDescription("*Oops!* \nThere was a error on fetching that channel! Please try again!\n\nChannel Example: *#giveaways-channel*")
-        }
-        if(!_channel) return message.reply("Specify a channel!");
-        const _item = args[1]
-        if(!_item) return message.reply("Specify a item!");
-        const _time = args[2]
-        if(!_time) return message.reply("Specify the time!");
+        const channel = message.mentions.channels.first() || args[0]
+        if(!channel) return message.reply("Specify a channel!");
+        const item = args[1]
+        if(!item) return message.reply("Specify a item!");
+        const time = args[2]
+        if(!time) return message.reply("Specify the time!");
         const embed = new Discord.RichEmbed()
         .setTitle("Giveaway! :tada:")
-        .setDescription(_item)
+        .setDescription(item)
         .setColor("#7cfc00")
-        .setFooter(`React 🎉 to join - ${_time}`)
+        .setFooter(`React 🎉 to join - ${time}`)
         .setTimestamp()
-        _channel.send(embed).then(msg => {
+        channel.send(embed).then(msg => {
             msg.react("🎉")
             setTimeout(() => {
                 const peopleReacted = msg.reactions.get("🎉").users;
                 var winner = peopleReacted.random()
-                if(winner.id === client.user.id) {
-                    winner = peopleReacted.random()
-                    if(winner.id === client.user.id) {
-                        winner = peopleReacted.random()
-                        _channel.send(`${winner} has won ${_item}! :tada:`)
+                        channel.send(`${winner} has won ${item}! :tada:`)
                         const finalE = new Discord.RichEmbed()
                         .setTitle("Giveaway Over")
                         .setColor("#7cfc00")
                         .setDescription(`Winner: ${winner}`)
                         msg.edit(finalE)
-                    }
-                    _channel.send(`${winner} has won ${_item}! :tada:`)
-                    const finalE = new Discord.RichEmbed()
-                    .setTitle("Giveaway Over")
-                    .setColor("#ff8c00")
-                    .setDescription(`Winner: ${winner}`)
-                    msg.edit(finalE)
-                }
-            }, ms(_time))
+            }, ms(time))
         })
-    } catch (error) {
-        const Error = new Discord.RichEmbed()
-        .setTitle("Error!")
-        .setDescription("Error! I've received this error: ```js \n" + error + "```")
-        .setTimestamp()
-        message.channel.send(Error)
-    }
     }
