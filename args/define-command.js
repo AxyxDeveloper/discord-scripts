f207429806c735776d3331c85cbe03346c5d084b
Ruben de Bruijne <49074962+AxyxDeveloper@users.noreply.github.com>
2020-03-13 10:41:07 +0100

    Delete define-command.js

--- args/define-command.js
+++
@@ -1,7 +0,0 @@
-//Used for args[] and if(command === "") {}
-    const args = message.content
-        .slice(prefix.length)
-        .trim()
-        .split(/ +/g);
-    const command = args.shift().toLowerCase();
-    if(message.author.bot) return;
