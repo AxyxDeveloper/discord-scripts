b4c5262c89089dc863eac4ec0bad6c30a77cffba
Cleetus <rubenjafethdebruijne@gmail.com>
2020-07-31 12:12:34 +0200

    Delete owner-shutdown.js

--- dbc/scripts/owner-shutdown.js
+++
@@ -1,6 +0,0 @@
-    if (command === "shutdown" && botConfigs.plugins[10].activated == true) {
-        if (!message.author.id==="yourid") return message.channel.send("You don't have permission!"); //replace "yourid" with your id lol
-        await message.channel.send(`Good night, ${message.author.tag}!`);
-        await message.delete().catch();
-        await process.exit().catch((e) => { console.error(e); });
-    }
