01be6910cd4288410b52e38860d92c3dc08b29b7
Cleetus <rubenjafethdebruijne@gmail.com>
2020-07-31 11:54:16 +0200

    Update timed-messages.js

--- timed-messages/timed-messages.js
+++ timed-messages/timed-messages.js
@@ -1,1 +1,20 @@
-
+client.on("ready", () => {
+    var timedmessages = {
+        "a random number, can't be the same from another. add this number to the \"Messages\"": {
+            "server": {
+                "guild": "your server id",
+                "channel": "the channel id"
+            },
+            "interval": "Time in MILLISECONDS",
+            "message": "your message here"
+        }
+    }
+    var messages = ["<the number>"]
+    messages.forEach(element => {
+        const guild = client.guilds.get(timedmessages[element].server.guild)
+        const channel = guild.channels.get(timedmessages[element].server.channel)
+        setInterval(() => {
+            channel.send(timedmessages[element].message)
+        }, timedmessages[element].interval)
+    })
+})
