738d9f8f996cee25f667cba23cfc9ffede930e1a
Dev <rubenjafethdebruijne@gmail.com>
2020-08-01 17:48:20 +0200

    Update webhookreply.js

--- Utility/webhookreply.js
+++ Utility/webhookreply.js
@@ -1,17 +1,6 @@
 
 //This is a hardcore bot for professionals. Please use this if you know wat this does.
-const Discord = require("discord.js")
-const client = new Discord.Client()
 
-client.on("message", async function (message) {
-    const prefix = ".";
-    const args = message.content
-        .slice(prefix.length)
-        .trim()
-        .split(/ +/g);
-    const command = args.shift().toLowerCase();
-    if(message.author.bot) return;
-    if(!message.content.startsWith === prefix) return;
     if (command === "say") {
         //script start
         const msg = message.content.split(' ').splice(1).join(' ');
@@ -33,6 +22,3 @@ client.on("message", async function (message) {
             //script end
         })
     }
-})
-
-client.login("token")
