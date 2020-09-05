a6fec0e895b78b7619bf166e16ab3bee5db1e4c3
Dev <rubenjafethdebruijne@gmail.com>
2020-09-05 13:49:22 +0200

    Update say-command.js

--- Misc/say-command.js
+++ Misc/say-command.js
@@ -1,16 +1,5 @@
-//This is a hardcore bot for professionals. Please use this if you know wat this does.
-const Discord = require("discord.js")
-const client = new Discord.Client()
 
-client.on("message", async function (message) {
-    const prefix = ".";
-    const args = message.content
-        .slice(prefix.length)
-        .trim()
-        .split(/ +/g);
-    const command = args.shift().toLowerCase();
-    if(message.author.bot) return;
-    if(!message.content.startsWith === prefix) return;
+
     if (command === "say") {
         //script start
             //remove the // from this script to enable admin only
@@ -23,6 +12,5 @@ client.on("message", async function (message) {
             //script end
         })
     }
-})
 
-client.login("token")
+          
